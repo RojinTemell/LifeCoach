2b834b15195cbbcfe710d681c715cad401a6ebd4
RojinTemell <rojintemel02@gmail.com>
2026-06-20 16:32:57 +0300

    mounted error

--- lib/features/dashboard/presentation/view/dashboard.dart
+++ lib/features/dashboard/presentation/view/dashboard.dart
@@ -19,18 +19,24 @@ class Dashboard extends StatelessWidget {
             const SizedBox(height: 16),
             ElevatedButton(
               onPressed: () async {
-                final messenger = ScaffoldMessenger.of(context);
-                final repo = getIt<HealthRepository>();
-                final req = await repo.requestPermission();
-                final steps = req ? await repo.getTodaySteps() : 0;
-                if (!context.mounted) {
+                final messenger = ScaffoldMessenger.of(
+                  context,
+                ); // await'ten ÖNCE
+                try {
+                  final repo = getIt<HealthRepository>();
+
+                  final req = await repo.requestPermission();
+
+                  final steps = req ? await repo.getTodaySteps() : 0;
+
                   messenger.showSnackBar(
-                    SnackBar(
-                      content: Text('İzin: $req · Bugünkü adım: $steps'),
-                    ),
+                    SnackBar(content: Text('İzin: $req · Adım: $steps')),
                   );
+                } on Exception catch (e) {
+                  messenger.showSnackBar(SnackBar(content: Text('HATA: $e')));
                 }
               },
+
               child: const Text('Sağlık iznini test et'),
             ),
             ElevatedButton(
